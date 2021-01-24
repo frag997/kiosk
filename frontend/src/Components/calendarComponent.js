c059b97e4e37092c481edf37ccbc98a7948b20be
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2021-01-24 08:20:59 -0800

    Feat add calendar dependency by hand

--- frontend/src/Components/calendarComponent.js
+++ frontend/src/Components/calendarComponent.js
@@ -1,5 +1,10 @@
-import React, {useState} from 'react'
-import Calendar from 'react_google_calendar'
+import React, { Component } from 'react'
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import googleAPI from  "../utils/googleAPI"
+
+const localizer = BigCalendar.momentLocalizer(moment)
 
 const calendar_configuration = {
     api_key: 'AIzaSyDLXd3SqI-pVmPdcfOE9I1_vCqQfXwP-4o',
@@ -14,14 +19,36 @@ const calendar_configuration = {
     monthlyRecurrence: 100
 }
 
-const CalendarComponent  = () => {
-   const { events } = useState([])
-   console.log('events', events)
-    return (
-        <Calendar
-        events={events}
-        config={calendar_configuration} />
-    )
-}
+export default class Calendar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      events: []
+    }
+  }
+  
 
-export default CalendarComponent;
+  componentDidMount = () => {
+    if (calendar_configuration) {
+      this.getGoogleCalendarEvents()
+    } else {
+      console.log("React Google Calendar requires you pass a configuration object")
+    }
+  }
+
+  getGoogleCalendarEvents = () => {
+    googleAPI.getAllCalendars(calendar_configuration)
+      .then(events => {
+        this.setState({ events })
+      })
+      .catch(err => { throw new Error(err) })
+  }
+
+  render = () =>
+    <div>
+      <BigCalendar
+        localizer={localizer}
+        events={this.state.events}
+        style={{ height: "100vh" }} />
+    </div>
+}
