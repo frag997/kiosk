0410fe762d2f9d5bb9ee930e5fb6bc3be5210cc8
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2021-03-02 19:30:25 -0800

    Updates

--- frontend/src/Containers/weatherContainer.js
+++
@@ -1,9 +0,0 @@
-export default function WeatherContainer () {
-
-    // USER POSTION
-    const lat = 33.350616; //position.coords.latitude;
-    const lon = -115.729622; //position.coords.longitude;
-
-    
-    return <h1>Hello from weather container!</h1>
-}
